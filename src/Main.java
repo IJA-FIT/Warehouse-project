3c37a89cd65650f85092abdd94a3954d0c057084
Vojtech Bubela <vojta@bubela.cz>
2021-05-08 05:11:20 +0200

    add trail drawing

--- src/Main.java
+++ src/Main.java
@@ -58,6 +58,7 @@ public class Main extends Application {
 
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
+        primaryStage.setTitle("Sklad");
         primaryStage.show();
         
         
